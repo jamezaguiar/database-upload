56f153bdc74b526b9bb766b42d02b6da98be4beb
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-10 23:36:23 -0300

    Listing transactions with balance

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -7,15 +7,15 @@ import CreateCategoryService from '../services/CreateCategoryService';
 @EntityRepository(Category)
 class CategoriesRepository extends Repository<Category> {
   public async verifyIfCategoryExists(title: string): Promise<Category> {
-    let findCategory = await this.findOne({ where: { title } });
+    let category = await this.findOne({ where: { title } });
 
-    if (!findCategory) {
+    if (!category) {
       const createCategory = new CreateCategoryService();
 
-      findCategory = await createCategory.execute({ title });
+      category = await createCategory.execute({ title });
     }
 
-    return findCategory;
+    return category;
   }
 }
 
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -17,12 +17,12 @@ class TransactionsRepository extends Repository<Transaction> {
       (accumulator: Balance, transaction: Transaction) => {
         switch (transaction.type) {
           case 'income':
-            accumulator.income += transaction.value;
-            accumulator.total += transaction.value;
+            accumulator.income += Number(transaction.value);
+            accumulator.total += Number(transaction.value);
             break;
           case 'outcome':
-            accumulator.outcome += transaction.value;
-            accumulator.total -= transaction.value;
+            accumulator.outcome += Number(transaction.value);
+            accumulator.total -= Number(transaction.value);
             break;
           default:
             break;
