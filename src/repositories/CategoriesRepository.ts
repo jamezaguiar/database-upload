e6b8521609f1dd36fcff9990961225fd89f3d825
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-11 00:28:23 -0300

    Capitalizing categories titles

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,5 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import capitalize from '../utils/capitalize';
+
 import Category from '../models/Category';
 
 import CreateCategoryService from '../services/CreateCategoryService';
@@ -7,7 +9,9 @@ import CreateCategoryService from '../services/CreateCategoryService';
 @EntityRepository(Category)
 class CategoriesRepository extends Repository<Category> {
   public async verifyIfCategoryExists(title: string): Promise<Category> {
-    let category = await this.findOne({ where: { title } });
+    const capitalizedTitle = capitalize(title);
+
+    let category = await this.findOne({ where: { title: capitalizedTitle } });
 
     if (!category) {
       const createCategory = new CreateCategoryService();
